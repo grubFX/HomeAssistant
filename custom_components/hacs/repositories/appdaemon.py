f5c66ab6238e583f712344b71e23b83261f67584
grubFX <2887162+grubFX@users.noreply.github.com>
2021-05-23 14:30:27 +0200

    MOD various updates, FIX light groups

--- custom_components/hacs/repositories/appdaemon.py
+++ custom_components/hacs/repositories/appdaemon.py
@@ -1,9 +1,8 @@
 """Class for appdaemon apps in HACS."""
 from aiogithubapi import AIOGitHubAPIException
 
+from custom_components.hacs.enums import HacsCategory
 from custom_components.hacs.helpers.classes.exceptions import HacsException
-from custom_components.hacs.helpers.functions.logger import getLogger
-
 from custom_components.hacs.helpers.classes.repository import HacsRepository
 
 
@@ -15,15 +14,14 @@ class HacsAppdaemon(HacsRepository):
         super().__init__()
         self.data.full_name = full_name
         self.data.full_name_lower = full_name.lower()
-        self.data.category = "appdaemon"
+        self.data.category = HacsCategory.APPDAEMON
         self.content.path.local = self.localpath
         self.content.path.remote = "apps"
-        self.logger = getLogger(f"repository.{self.data.category}.{full_name}")
 
     @property
     def localpath(self):
         """Return localpath."""
-        return f"{self.hacs.system.config_path}/appdaemon/apps/{self.data.name}"
+        return f"{self.hacs.core.config_path}/appdaemon/apps/{self.data.name}"
 
     async def validate_repository(self):
         """Validate."""
@@ -35,7 +33,7 @@ class HacsAppdaemon(HacsRepository):
         except AIOGitHubAPIException:
             raise HacsException(
                 f"Repostitory structure for {self.ref.replace('tags/','')} is not compliant"
-            )
+            ) from None
 
         if not isinstance(addir, list):
             self.validate.errors.append("Repostitory structure not compliant")
@@ -48,13 +46,14 @@ class HacsAppdaemon(HacsRepository):
         # Handle potential errors
         if self.validate.errors:
             for error in self.validate.errors:
-                if not self.hacs.system.status.startup:
-                    self.logger.error(error)
+                if not self.hacs.status.startup:
+                    self.logger.error("%s %s", self, error)
         return self.validate.success
 
-    async def update_repository(self, ignore_issues=False):
+    async def update_repository(self, ignore_issues=False, force=False):
         """Update."""
-        await self.common_update(ignore_issues)
+        if not await self.common_update(ignore_issues, force):
+            return
 
         # Get appdaemon objects.
         if self.repository_manifest:
